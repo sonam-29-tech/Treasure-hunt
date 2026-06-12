8cdb2b180dd0e316ee8a9e059ae2539a34402b77
sonam-29-tech <sainianuradha2708@gmail.com>
2026-06-12 12:59:11 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,16 @@
+#Variables   we can name our variables anything but try to make sense we can use multiple words using underscore
+# Numbers can't be written in the starting of name like name1 is write but 1name is wrong
+# Don't use name of functions or print coz it shows error
+name=input("What's your name?")
+print(name)
+a= "SONAM"
+print(a)
+print(len(a))
+print(len(input("What's your name?")))  # It will tell the length of what we get from input function
+# We can also write it like this using several steps
+username=input("What's your name?")
+length =len(username)
+print(length)
+
+
+
